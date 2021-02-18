f2c8d4b933ce6f552e61f498f18cbbb5a82f9851
Joel Naarendorp <35632308+N3RDCLASH@users.noreply.github.com>
2021-02-18 11:31:02 -0300

    added tabs to users page

--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -5,16 +5,24 @@
 
 @section('content')
 <div class="content">
-    <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <a class="nav-link active" href="#">Admin</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Studenten</a>
-        </li>
-        <li class="nav-item">
-        </li>
-    </ul>
+    <nav>
+        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#admins" role="tab"
+                aria-controls="nav-home" aria-selected="true">Admin</a>
+            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#studenten" role="tab"
+                aria-controls="nav-profile" aria-selected="false">Studenten</a>
+        </div>
+    </nav>
+    <div class="tab-content" id="nav-tabContent">
+        <div class="tab-pane fade show active" id="admin" role="tabpanel" aria-labelledby="nav-home-tab">
+            {{-- admin stuff here --}}
+            tralala
+        </div>
+        <div class="tab-pane fade" id="studenten" role="tabpanel" aria-labelledby="nav-profile-tab">
+            {{-- student stuff here --}}
+            bidnfdfjdf
+        </div>
+    </div>
 </div>
 @endsection
 
